a56cd0395c48d621c909c6186d9b05f5fb41127e
gitsPBP <george@poweredbypeople.com>
2023-02-13 19:36:02 +0300

    Update env

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 7700;
+const PORT = process.env.PORT || 9000;
 
 app.listen(
   PORT,
