49c840107b5e6ceba58b032f12b953aa6595c0a0
gitsPBP <george@poweredbypeople.com>
2023-02-13 16:49:51 +0300

    Changed start script to npm start

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -26,6 +26,7 @@ const HomeScreen = () => {
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
+          <
           {products?.map((product) => (
             <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
               <Product product={product} />
