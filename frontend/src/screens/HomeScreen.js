f178aab568869337b994df1a58aee727df43504d
gitsPBP <george@poweredbypeople.com>
2023-02-13 17:08:54 +0300

    Removed error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -26,7 +26,6 @@ const HomeScreen = () => {
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
-          <
           {products?.map((product) => (
             <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
               <Product product={product} />
