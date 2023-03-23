3dcab3da5600f42c2c29167d279a9653d6208e03
gitsPBP <george@poweredbypeople.com>
2023-03-23 12:22:03 +0300

    Added origin on backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000", 
+  credentials: true,
+}));
 
 app.get("/", (req, res) => {
   res.send("API is Running");
